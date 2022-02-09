f2e83e461840b1b7764a2fe3c69c4b2f7a2330ce
Monika Sliciute <sliciute.monika@gmail.com>
2022-02-09 00:39:11 +0000

    home page row 1 layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart' as color;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 70,
+          left: 30,
+          right: 30,
+        ),
+        child: Column(
+          children: [
+            Row(
+              // Row 1
+              children: [
+                Text(
+                  'Training',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: color.AppColor.homePageTitle,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Expanded(child: Container()), // spacer between text and icons
+                Icon(
+                  // icon 1
+                  Icons.arrow_back_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+                SizedBox(width: 10), // spacer between icons 1 and 2
+                Icon(
+                  // icon 2
+                  Icons.calendar_today_outlined,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+                SizedBox(width: 15), // spacer between icons 2 and 3
+                Icon(
+                  // icons 3
+                  Icons.arrow_forward_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
